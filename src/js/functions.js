9bece763bef4cad17621f27a7775ff3b4b712579
NeftaliGC <aliguiacruz@outlook.com>
2024-05-18 11:22:37 -0600

    nav buttons ready

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,50 +1,44 @@
-const answer = document.querySelector('.answer');
-const queson = document.getElementById('queson');
-const comfunciona = document.getElementById('comfunciona');
-
-
-function toggleAnswer(answer) {
-    // Verificar el estado actual y alternar
-    if (window.getComputedStyle(answer).getPropertyValue('display') === 'none') {
-        answer.style.display = 'block'; // Mostrar la respuesta
-    } else {
-        answer.style.display = 'none'; // Ocultar la respuesta
-    }
-}
-
-
-function getState() {
-    return window.getComputedStyle(answer).getPropertyValue('display');
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const questions = document.querySelectorAll('.question');
+    
+    function toggleAnswer(answer) {
+        // Verificar el estado actual y alternar
+        if (window.getComputedStyle(answer).getPropertyValue('display') === 'none') {
+            answer.style.display = 'block'; // Mostrar la respuesta
+        } else {
+            answer.style.display = 'none'; // Ocultar la respuesta
+        }
+    }
+    
+    function getState(answer) {
+        return window.getComputedStyle(answer).getPropertyValue('display');
+    }
     
     questions.forEach(question => {
         question.addEventListener('click', () => {
             const answer = question.querySelector('.answer');
             toggleAnswer(answer);
-            console.log("estado:" + getState());
+            console.log("estado:" + getState(answer));
         });
     });
-    
-    function open(btn) {
-        console.log("estado en if:" + getState());
-        
-        if (getState() === 'none') {
-            btn.onclick = () => {
-                questions.forEach(question => {
-                    question.click();
-                });
-            };
+
+    function open() {
+        const firstAnswerState = getState(questions[0].querySelector('.answer'));
+        const secondAnswerState = getState(questions[1].querySelector('.answer'));
+        console.log("estado en if: " + firstAnswerState);
+        if (firstAnswerState === 'none' || secondAnswerState === 'none') {
+            questions.forEach(question => {
+                const answer = question.querySelector('.answer');
+                if (getState(answer) === 'none') {
+                    toggleAnswer(answer);
+                }
+            });
         }
     }
     
-    queson.onclick = () => {
-        open(queson);
-    };
+    const queson = document.getElementById('queson');
+    const comfunciona = document.getElementById('comfunciona');
     
-    comfunciona.onclick = () => {
-        open(comfunciona);
-    };
+    queson.addEventListener('click', open);
+    comfunciona.addEventListener('click', open);
 });
